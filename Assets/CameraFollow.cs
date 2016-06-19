cf2251c0ee2c5b769d0612f792d6096952c064e6
Pierre Depaz <pierre.depaz@gmail.com>
2016-06-19 15:32:15 -0400

    colliders and camera

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public GameObject targetCharacter;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt (targetCharacter.transform);
+	}
+}
