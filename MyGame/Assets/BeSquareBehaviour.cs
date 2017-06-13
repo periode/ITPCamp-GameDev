652a180a7f6f3b170f491e3e790dec62c4bd2e21
Pierre Depaz <pierre.depaz@gmail.com>
2017-06-13 14:59:01 -0400

    added scripts

---
+++ MyGame/Assets/BeSquareBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeSquareBehaviour : MonoBehaviour {
+
+	float movementSpeed = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//if the user is pressing the W key
+		if(Input.GetKey(KeyCode.W)){
+			this.transform.Translate (Vector3.forward * movementSpeed);
+		}
+
+		if(Input.GetKey(KeyCode.S)){
+			this.transform.Translate (Vector3.back * movementSpeed);
+		}
+
+		//if I press A go left
+		if(Input.GetKey(KeyCode.A)){
+			this.transform.Translate (Vector3.left * movementSpeed);
+		}
+
+		//if I press D go right
+		if(Input.GetKey(KeyCode.D)){
+			this.transform.Translate (Vector3.right * movementSpeed);
+		}
+	}
+}
