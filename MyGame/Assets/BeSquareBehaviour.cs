434ebecf36a281996ac26c41c32b77d60eb24be5
Pierre Depaz <pierre.depaz@gmail.com>
2017-06-13 15:34:22 -0400

    music

--- MyGame/Assets/BeSquareBehaviour.cs
+++ MyGame/Assets/BeSquareBehaviour.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BeSquareBehaviour : MonoBehaviour {
 
-	float movementSpeed = 0.5f;
+	public float movementSpeed = 0.5f;
 
 	// Use this for initialization
 	void Start () {
